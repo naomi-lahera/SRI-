ac52bd6e6a5688fbec35f4255e4c5f7053714a31
Naomi Lahera <lahera0103@gmail.com>
2024-02-12 22:43:49 -0500

    funcional

--- front/src/app/components/body/body.component.ts
+++ front/src/app/components/body/body.component.ts
@@ -27,63 +27,63 @@ export class BodyComponent {
   query!: string;
   total = 0;
   source!: string;
-  searchResult!: SerachResult; 
-  // = 
-  // {
-  //   base:
-  //   {
-  //     title: "Title  1",
-  //     author: "Author  1",
-  //     summary: "Summary of the first news item.",
-  //     date: new Date("2024-02-08"),
-  //     url: "https://example.com/news1"
-  //   },
-  //   associated_news: 
-  // [
-  //     {
-  //       title: "Title  1",
-  //       author: "Author  1",
-  //       summary: "Summary of the first news item.",
-  //       date: new Date("2024-02-08"),
-  //       url: "https://example.com/news1"
-  //     },
-  //     {
-  //       title: "Title  2",
-  //       author: "Author  2",
-  //       summary: "Summary of the second news item.",
-  //       date: new Date( "2024-02-07"),
-  //       url: "https://example.com/news2"
-  //     },
-  //     {
-  //       title: "Title  3",
-  //       author: "Author  3",
-  //       summary: "Summary of the third news item.",
-  //       date: new Date( "2024-02-06"),
-  //       url: "https://example.com/news3"
-  //     },
-  //     {
-  //       title: "Title  4",
-  //       author: "Author  4",
-  //       summary: "Summary of the fourth news item.",
-  //       date: new Date( "2024-02-05"),
-  //       url: "https://example.com/news4"
-  //     },
-  //     {
-  //       title: "Title  5",
-  //       author: "Author  5",
-  //       summary: "Summary of the fifth news item.",
-  //       date: new Date("2024-02-04"),
-  //       url: "https://example.com/news5"
-  //     },
-  //     {
-  //       title: "Title  6",
-  //       author: "Author  6",
-  //       summary: "Summary of the sixth news item.",
-  //       date: new Date("2024-02-03"),
-  //       url: "https://example.com/news6"
-  //     }
-  //   ]
-  // };
+  searchResult: SerachResult 
+  = 
+  {
+    base:
+    {
+      title: "Title  1",
+      author: "Author  1",
+      summary: "Summary of the first news item.",
+      date: new Date("2024-02-08"),
+      url: "https://example.com/news1"
+    },
+    associated_news: 
+  [
+      {
+        title: "Title  1",
+        author: "Author  1",
+        summary: "Summary of the first news item.",
+        date: new Date("2024-02-08"),
+        url: "https://example.com/news1"
+      },
+      {
+        title: "Title  2",
+        author: "Author  2",
+        summary: "Summary of the second news item.",
+        date: new Date( "2024-02-07"),
+        url: "https://example.com/news2"
+      },
+      {
+        title: "Title  3",
+        author: "Author  3",
+        summary: "Summary of the third news item.",
+        date: new Date( "2024-02-06"),
+        url: "https://example.com/news3"
+      },
+      {
+        title: "Title  4",
+        author: "Author  4",
+        summary: "Summary of the fourth news item.",
+        date: new Date( "2024-02-05"),
+        url: "https://example.com/news4"
+      },
+      {
+        title: "Title  5",
+        author: "Author  5",
+        summary: "Summary of the fifth news item.",
+        date: new Date("2024-02-04"),
+        url: "https://example.com/news5"
+      },
+      {
+        title: "Title  6",
+        author: "Author  6",
+        summary: "Summary of the sixth news item.",
+        date: new Date("2024-02-03"),
+        url: "https://example.com/news6"
+      }
+    ]
+  };
 
   search() {
     // console.log('component searching for:', this.query);
